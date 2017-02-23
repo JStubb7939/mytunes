92a1532e9b266649db108779c7e98d8f7457a270
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-02-22 20:50:29 -0500

    Complete Mocha tests up to the FETCH part

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,27 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    this.on('add', function(){
+      if (this.length === 1){
+        this.playFirst();
+      }
+    }, this),
 
+    this.on('remove', function(){
+      if (this.length>0){
+        this.playFirst();
+      }
+    }, this),
+
+    this.on('dequeue', function(song){
+      this.remove(song);
+    }, this),
+
+    this.on('ended', function(song){
+      this.at(0).dequeue();
+    }, this)
+},
+    playFirst: function(){
+      this.at(0).play();
+    }
 });
