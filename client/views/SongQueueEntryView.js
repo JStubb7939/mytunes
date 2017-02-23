c3e64d65b5d5ee6a7ba94ce064020bd64544bc13
Jordan Stubblefield <JStubblefield7939@gmail.com>
2017-02-22 20:47:36 -0500

    Complete mocha tests for views

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,21 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
-  // your code here!
+
+  tagname: 'tr',
+
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+  events: {
+    // 'add': function() {
+    //  this.render();
+    // },
+    // 'remove': function() {
+    //   this.render();
+    //}
+  },
+
+  render: function() {
+    console.log("Testing");
+    return this.$el.html(this.template(this.model.attributes));
+  }
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,10 +2,20 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.render();
+    this.collection.on('add', function(){ this.render(); }, this);
+    this.collection.on('remove', function(){ this.render(); }, this);
+
+
+
   },
 
   render: function() {
-    return this.$el;
+    // this.$el.children().detach();
+    return this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
-
 });
